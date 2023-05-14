bc7252b610be0dcb2d8c99ed8aee00ecfba86eb0
jvtipaymerantes <emgbbiaco26@gmail.com>
2023-05-14 16:32:16 +0800

    deployment

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,12 @@ import { auth } from './firebase'
 import { signOut } from 'firebase/auth'
 import './App.css'
 
-import Register from './components/Register'
+import Register from './pages/Register'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
-import Home from './components/Home'
-import About from './components/About'
-import Team from './components/Team'
+import Home from './pages/Home'
+import About from './pages/About'
+import Team from './pages/Team'
 
 
 function App() {
